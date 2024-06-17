2540c642b3d7a962d424a437a391bc4f8c49e03f
caspergtag <109670694+nonNamedUser@users.noreply.github.com>
2024-06-17 17:10:06 +0200

    Create Settings.lua

---
+++ AdminCommands/Config/Settings.lua
@@ -0,0 +1,19 @@
+local settings={}
+
+settings.Prefix="!"
+
+settings.UserIds={
+	556101408, -- SnowBoy12766
+
+}
+
+settings.whitelistEnabled = false
+
+settings.groupEnabled=false
+
+settings.groupInfo={
+	['GroupId'] = 0,
+	['MemberIds'] = {254,255}
+}
+
+return settings
